44f7b235f76fe99517a2ad7eeca0723ebd46d4f6
DESKTOP-4SFC0M1\HP <toannvph25194@fpt.edu.vn>
2024-01-09 16:15:47 +0700

    Đẩy project có thư viện và kết nối db lên

---
+++ be_duantn/src/main/java/com/example/be_duantn/BeDuantnApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_duantn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeDuantnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeDuantnApplication.class, args);
+    }
+
+}
---
+++ be_duantn/src/test/java/com/example/be_duantn/BeDuantnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.be_duantn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeDuantnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
